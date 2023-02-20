3a29adfd4bacdd7e1f94e0905f54d82eae901d5b
Serey Roth <sereyratanakroth@gmail.com>
2023-02-20 14:36:28 -0800

    Implement new PanelGroup component

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,13 +1,14 @@
 import Panel from '~/components/shared/panel';
+import { PanelGroup } from '~/components/shared/PanelGroup';
 
 export default function Index() {
     return (
         <div className="flex h-full w-full flex-col">
             <div className="h-16 flex-shrink border-b border-b-gray-200"></div>
-            <div className="flex-shring h-8 border-b border-b-gray-200"></div>
-            <div className="flex h-full flex-grow flex-row">
-                <div className="w-56 flex-shrink border-r border-r-gray-200">
-                    <Panel title="Left panel">
+            <div className="flex-shrink h-8 border-b border-b-gray-200"></div>
+            <div className="flex h-full flex-grow flex-row relative">
+                <PanelGroup side="left" active={1}>
+                    <Panel title="Panel 1">
                         <h2 className="font-bold">Lorem ipsum</h2>
                         <p>Lorem ipsum dolor sit amet...</p>
                         <table>
@@ -25,10 +26,28 @@ export default function Index() {
                             </tbody>
                         </table>
                     </Panel>
-                </div>
+                    <Panel title="Panel 2">
+                        <h2 className="font-bold">Lorem ipsum</h2>
+                        <p>Lorem ipsum dolor sit amet...</p>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>List</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {[1, 2, 3, 4].map((i) => (
+                                    <tr key={i}>
+                                        <td>Item {i}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </Panel>
+                </PanelGroup>
                 <div className="flex-grow bg-gray-50"></div>
-                <div className="w-56 flex-shrink border-l border-l-gray-200">
-                    <Panel title="Right panel">
+                <PanelGroup side="right" active={1}>
+                    <Panel title="Panel 3">
                         <h2 className="font-bold">Lorem ipsum</h2>
                         <p>Lorem ipsum dolor sit amet...</p>
                         <table>
@@ -46,7 +65,25 @@ export default function Index() {
                             </tbody>
                         </table>
                     </Panel>
-                </div>
+                    <Panel title="Panel 4">
+                        <h2 className="font-bold">Lorem ipsum</h2>
+                        <p>Lorem ipsum dolor sit amet...</p>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>List</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {[1, 2, 3, 4].map((i) => (
+                                    <tr key={i}>
+                                        <td>Item {i}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </Panel>
+                </PanelGroup>
             </div>
         </div>
     );
