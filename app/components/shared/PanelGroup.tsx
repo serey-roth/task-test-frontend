1873dce6168292a5e028a246a8270b72dff0752c
Serey Roth <sereyratanakroth@gmail.com>
2023-02-20 15:01:25 -0800

    Update PanelGroup's styles and add transitions

--- app/components/shared/PanelGroup.tsx
+++ app/components/shared/PanelGroup.tsx
@@ -3,6 +3,22 @@ import { Children, useState } from 'react';
 
 type Side = "left" | "right";
 
+function getTransitionClasses(
+    isOpen: boolean, 
+    side: Side
+) {
+    let classes = "";
+    if (isOpen) {
+        classes += "translate-x-0 opacity-100";
+    } else {
+        classes += "opacity-0 " + 
+        (side === "left" ? 
+        "-translate-x-full" : 
+        "translate-x-full");
+    }
+    return classes;
+}
+
 interface PanelGroupProps {
     side: Side;
     active: number;
@@ -28,48 +44,53 @@ export function PanelGroup({
     return (
         <div
         className={`absolute top-0 bottom-0 flex
-        h-full flex-row bg-white ${side === 'left' ? 'left-0' : 'right-0'}`}
+        h-full flex-row ${side === 'left' ? 
+        'left-0' : 'right-0'}`}
         >
-            {isOpen ? (
-                <div
-                className="flex w-56 flex-shrink flex-col
-                overflow-auto border-r border-r-gray-200 p-2"
-                >
-                    <div className="flex flex-row items-center justify-between">
-                        <select 
-                        value={currentActive} 
-                        onChange={handleSelectPanel}>
-                            {Children.map(children, (_, index) => (
-                                <option 
-                                key={`panel-${index}`} 
-                                value={index + 1}>
-                                    Panel {index + 1}
-                                </option>
-                            ))}
-                        </select>
-                        <button onClick={togglePanelGroup}>X</button>
-                    </div>
-                    {Children.toArray(children)[currentActive - 1]}
+            <div
+            className={`flex w-56 flex-shrink flex-col
+            gap-2 ${getTransitionClasses(isOpen, side)}
+            transition duration-1000 ease-in-out bg-white
+            ${side === "left" ? "border-r" : "border-l"}
+            overflow-auto border-r border-r-gray-200 p-2`}
+            >
+                <div className="flex flex-row items-center 
+                justify-between gap-2">
+                    <select 
+                    className='flex-1'
+                    value={currentActive} 
+                    onChange={handleSelectPanel}>
+                        {Children.map(children, (_, index) => (
+                            <option key={`panel-${index}`} value={index + 1}>
+                                Panel {index + 1}
+                            </option>
+                        ))}
+                    </select>
+                    <button onClick={togglePanelGroup}>X</button>
                 </div>
-            ) : (
-                <div className="w-12 flex-shrink border-r border-r-gray-200">
-                    <button 
-                    className="w-full border-b drop-shadow-sm" 
-                    onClick={togglePanelGroup}>
-                        <span
-                        style={{
-                            writingMode: 'vertical-lr',
-                            msWritingMode: 'vertical-lr',
-                            WebkitWritingMode: 'vertical-lr'
-                        }}
-                        className={`${side === 'left' && 'rotate-180'}
-                        inline-block whitespace-nowrap py-1`}
-                        >
-                            Panel {active}
-                        </span>
-                    </button>
-                </div>
-            )}
+                {Children.toArray(children)[currentActive - 1]}
+            </div>
+            <div className={`absolute top-0 bottom-0 w-12 flex-shrink  
+            ${side === "left" ? "left-0 border-r" : 
+            "right-0 border-l"} ${isOpen ? "opacity-0" : 
+            "opacity-100"} transition duration-500 ease-in-out
+            bg-white border-gray-200`}>
+                <button 
+                className="w-full border-b drop-shadow-sm" 
+                onClick={togglePanelGroup}>
+                    <span
+                    style={{
+                        writingMode: 'vertical-lr',
+                        msWritingMode: 'vertical-lr',
+                        WebkitWritingMode: 'vertical-lr'
+                    }}
+                    className={`${side === 'left' && 'rotate-180'}
+                    inline-block whitespace-nowrap py-1`}
+                    >
+                        Panel {active}
+                    </span>
+                </button>
+            </div>
         </div>
     );
 }
